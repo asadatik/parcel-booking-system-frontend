4bc36b0975ce9989a1a21ad0b05d056ade57c768
Asad Atik <asadatik1995@gmail.com>
2025-08-29 17:37:05 +0600

    Managing Status Codes and Fixing Credential Mismatch Issues

--- src/components/modules/Authentication/LoginForm.tsx
+++ src/components/modules/Authentication/LoginForm.tsx
@@ -24,14 +24,28 @@ export function LoginForm({
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     try {
       const res = await login(data).unwrap();
-      console.log(res);
+
+    
+      if (res.success) {
+        toast.success("Logged in successfully");
+        navigate("/");
+      }  console.log(res);
+
+
     } catch (err) {
       console.error(err);
+   //
+      if (err.data.message === "Password does not match") {
+        toast.error("Invalid credentials");
+      }
 
-      if (err.status === 401) {
+      if (err.data.message === "User is not verified") {
         toast.error("Your account is not verified");
         navigate("/verify", { state: data.email });
       }
+
+     //  
+
     }
   };
 
