45dc6dfce0eaa08fe7208e821c7298febb7fe9d6
Asad Atik <asadatik1995@gmail.com>
2026-04-03 23:48:15 +0600

    turn into light theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import CommonLayout from './components/layout/CommonLayout'
 
 
+
 function App() {
 
 
--- src/context/theme.context.ts
+++ src/context/theme.context.ts
@@ -1,15 +1,15 @@
-import  type { Theme } from "@/providers/theme.provider";
-import { createContext } from "react";
+// import  type { Theme } from "@/providers/theme.provider";
+// import { createContext } from "react";
 
-type ThemeProviderState = {
-  theme: Theme;
-  setTheme: (theme: Theme) => void;
-};
+// type ThemeProviderState = {
+//   theme: Theme;
+//   setTheme: (theme: Theme) => void;
+// };
 
-const initialState: ThemeProviderState = {
-  theme: "system",
-  setTheme: () => null,
-};
+// const initialState: ThemeProviderState = {
+//   theme: "system",
+//   setTheme: () => null,
+// };
 
-export const ThemeProviderContext =
-  createContext<ThemeProviderState>(initialState);
+// export const ThemeProviderContext =
+//   createContext<ThemeProviderState>(initialState);
